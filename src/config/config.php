0f5b7f5ef77905b47f9303c5ae6b750c326ebef9
yuanzhihai <396751927@qq.com>
2023-02-23 15:55:36 +0800

    update README.md

--- src/config/config.php
+++ src/config/config.php
@@ -4,12 +4,13 @@
  * 配置文件
  */
 return [
-    'scheme'          => 'smtp',// "smtps": using TLS, "smtp": without using TLS.
-    'host'            => '', // 服务器地址
-    'username'        => '',
-    'password'        => '', // 密码
-    'port'            => 465, // SMTP服务器端口号,一般为25
-    'options'         => [], // See: https://symfony.com/doc/current/mailer.html#tls-peer-verification
-     // 'dsn'             => '',
-    'embed'           => 'cid:', // 邮件中嵌入图片元数据标记
+    'scheme'   => 'smtp',// "smtps": using TLS, "smtp": without using TLS.
+    'host'     => '', // 服务器地址
+    'username' => '',
+    'password' => '', // 密码
+    'port'     => 465, // SMTP服务器端口号,一般为25
+    'options'  => [], // See: https://symfony.com/doc/current/mailer.html#tls-peer-verification
+    // 'dsn'             => '',
+    'embed'    => 'cid:', // 邮件中嵌入图片元数据标记
+    'debug'    => false, // 开启debug模式会直接抛出异常
 ];
